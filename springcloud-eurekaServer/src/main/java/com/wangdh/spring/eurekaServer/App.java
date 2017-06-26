ff02fc86ea36d976d4e4f47b92eb7c86d7b6bf6c
王东鸿 <donghong.wang@frontpay.cn>
2017-06-26 17:50:16 +0800

    添加服务注册中心

---
+++ springcloud-eurekaServer/src/main/java/com/wangdh/spring/eurekaServer/App.java
@@ -0,0 +1,16 @@
+package com.wangdh.spring.eurekaServer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	new SpringApplicationBuilder(App.class).web(true).run(args);
+    	// SpringApplication.run(App.class, args);
+    }
+}
